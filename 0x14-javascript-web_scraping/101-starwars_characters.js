ed958dcf9f66d50c2f2b69903ca8b7d717f362b5
Precious <ikpaprecious2@gmail.com>
2023-04-25 11:24:51 -0700

    fix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-<<<<<<< HEAD
-const url ='https://swapi-api.hbtn.io/api/films/'+ process.argv[2];
-=======
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
->>>>>>> 7db4ed32abd4e06763b1acd86e920e7df688c6f7
+const url = 'https://swapi-api.alx-tools.com/api/films/'+ process.argv[2];
 request(url, function (error, response, body) {
   if (!error) {
     const characters = JSON.parse(body).characters;
